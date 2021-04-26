2b1e698bd140d1aab8096e512cc30db7d82102df
skyire <82963888+skyire@users.noreply.github.com>
2021-04-26 11:17:54 -0700

    Update pongbutorganized.py

--- pongbutorganized.py
+++ pongbutorganized.py
@@ -5,10 +5,15 @@ from turtle import *
 
 #screen setup
 screen = turtle.Screen()
+screen.setup(width=800, height=600)
 turtle.title('Pong but bad')
 color('black')
-turtle.shape('square')
 turtle.bgcolor('grey')
+turtle.penup()
+
+#ball setup
+ball = turtle.Turtle()
+ball.shape('square')
 turtle.penup()
 
 #player setup
@@ -57,17 +62,54 @@ screen.onkeypress(roboUp, 'Up')
 screen.onkeypress(roboDown, 'Down')
 
 #ball
-puckX = 0
-puckY = 0
-while 0==0:
-    currentBorder = 300
-    while puckX < currentBorder:
-        puckX = puckX+10
-        setx(puckX)
-    currentBorder = -300
-    while puckX >= currentBorder:
-        puckX = puckX-10
-        setx(puckX)
+ballX = 0
+ballY = 0
+#    currentBorder = 300
+#    while ballX < currentBorder:
+#        ballX = ballX+10
+#        setx(ballX)
+#    currentBorder = -300
+#    while ballX >= currentBorder:
+#        ballX = ballX-10
+#        setx(ballX)
+
+velX = 2
+velY = 2
+true = 1
+
+#game loop
+while true == 1:
+    ballX = ballX + velX
+    ballY = ballY + velY
+
+    #border control
+    if -290 < ball.xcor() < 290 and -290 < ball.ycor() <290:
+        ball.setx(ballX)
+        ball.sety(ballY)
+        print("if loop ran")
+        
+    elif ball.ycor() >= 290:
+        velY = velY*-1
+        ball.sety(290)
+        print("1")
+
+    elif ball.xcor() >= 290:
+        velX = velX*-1
+        ball.setx(290)
+        print("2")
+
+    elif ball.ycor() <= -290:
+        velY = velY*-1
+        ball.sety(-290)
+        print("3")
+
+    elif ball.xcor() <= -290:
+        velX = velX*-1
+        ball.setx(-290)
+        print("4")
+    print(str(ball.xcor()))
     
+
+        
 
 #done()
