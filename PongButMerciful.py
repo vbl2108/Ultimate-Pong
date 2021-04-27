4dc9e8a4aa2dd80086fcefd358ec072ffa3776ce
vbl2108 <82963975+vbl2108@users.noreply.github.com>
2021-04-27 16:02:34 -0700

    Add files via upload

---
+++ PongButMerciful.py
@@ -0,0 +1,166 @@
+# Made in collaboration between Carys Crewse and Vincent LeBaron
+# Turtle documentation found here:  https://docs.python.org/3/library/turtle.html#module-turtle
+# Random documentation found here: https://docs.python.org/3/library/random.html
+
+import turtle
+import random
+
+from turtle import *
+
+#screen setup
+screen = turtle.Screen()
+turtle.title('Extreme Pong: Multiplayer Edition')
+turtle.bgcolor('black')
+
+#score setup
+lscore = 0
+rscore = 0
+score = turtle.Turtle()
+score.color = ('black')
+score.hideturtle()
+score.penup()
+score.setposition(-330, 270)
+score.pendown()
+score.pencolor('green')
+score.write('Controls: W,S left Paddle | Up,Down right Paddle')
+score.penup()
+score.setposition(0, 240)
+score.pendown()
+score.write(str(lscore) + ' |Score| ' + str(rscore), False, align="center", font=('Arial', 25))
+
+
+#ball setup
+ball = turtle.Turtle()
+ball.shape('square')
+ball.color('purple')
+ball.penup()
+
+#player setup
+rightplayer = turtle.Turtle()
+rightplayer.penup()
+rightplayer.color('green')
+rightplayer.setposition(300, 0)
+rightplayer.shape('square')
+rightplayer.shapesize(5, 1)
+
+
+#leftplayer setup
+leftplayer = turtle.Turtle()
+leftplayer.penup()
+leftplayer.color('green')
+leftplayer.setposition(-300,0)
+leftplayer.shape('square')
+leftplayer.shapesize(5, 1)
+
+
+#move functions
+def rightplayerDown():
+    ry = rightplayer.ycor()
+    ry = ry-20
+    rightplayer.sety(ry)
+
+def rightplayerUp():
+    ry = rightplayer.ycor()
+    ry = ry+20
+    rightplayer.sety(ry)
+
+def leftplayerDown():
+    ly = leftplayer.ycor()
+    ly = ly-20
+    leftplayer.sety(ly)
+
+def leftplayerUp():
+    ly = leftplayer.ycor()
+    ly = ly+20
+    leftplayer.sety(ly)
+
+#key bindings 
+screen.listen()
+screen.onkeypress(leftplayerUp, 'w')
+screen.onkeypress(leftplayerDown, 's')
+
+screen.onkeypress(rightplayerUp, 'Up')
+screen.onkeypress(rightplayerDown, 'Down')
+
+#ball velocity on both axis. Random number between 1 and 5
+ball.velx = random.randrange(1, 4)
+ball.vely = random.randrange(1, 4)
+
+while 0==0:
+    #random number for random things
+    posNeg = random.randrange(1, 3)
+    print(posNeg)
+    #puck movement
+    ball.setx (ball.xcor() + ball.velx)
+    ball.sety (ball.ycor() + ball.vely)
+
+
+    #Border collision 
+    if ball.ycor() > 280:
+        #direction change  
+        ball.vely = -2
+
+        #velocity change
+        ball.velx = random.randrange(3, 5)
+        ball.vely = random.randrange(3, 5)
+        ball.vely = ball.vely*-1
+
+    if ball.ycor() < -280:
+        ball.vely = 3
+
+        ball.velx = random.randrange(3, 5)
+        ball.vely = random.randrange(3, 5)
+
+    if ball.xcor() > 320:
+        ball.setposition(0, 0)
+
+        ball.velx = random.randrange(3, 5)
+        ball.vely = random.randrange(3, 5)
+
+        #trying to make ball change direction after restart
+        if posNeg == 1:
+
+            ball.velx = ball.velx*-1
+            ball.vely = ball.vely*-1
+        else:
+            ball.velx = ball.velx*1
+            ball.vely= ball.vely*1
+
+        lscore = lscore+1
+        score.clear()
+        score.write(str(lscore) + ' |Score| ' + str(rscore), False, align="center", font=('Arial', 25))
+
+    if ball.xcor() < -320:
+        ball.setposition(0, 0)
+
+        ball.velx = random.randrange(1, 5)
+        ball.vely = random.randrange(1, 5)
+
+
+        if posNeg == 1:
+
+            ball.velx = ball.velx*-1
+            ball.vely = ball.vely*-1
+        elif posNeg == 2:
+            ball.velx = ball.velx*1
+            ball.vely= ball.vely*1
+
+
+        rscore = rscore+1
+        score.clear()
+        score.write(str(lscore) + ' |Score| ' + str(rscore), False, align="center", font=('Arial', 25))
+
+    #Paddle collision
+    def collideTrue():
+        ball.ycor() >= (rightplayer.ycor() - 45) and ball.ycor() <= (rightplayer.ycor() + 45)
+
+    if ball.xcor() >= 300 and collideTrue:
+        ball.velx = ball.velx *-1
+
+    if ball.xcor() <= -300 and collideTrue:
+        ball.velx = ball.velx *-1
+
+
+
+
+done()
